a9b922df92df6279bb8bc053751b40979fa972e7
Marko Gajic <marko.gajic@developion.com>
2023-02-27 10:23:25 -0100

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+function findEnv() {
+	return `${findSite()}/.env`
+}
+
+function findInParent({
+	fileOrDir = '',
+	parent = '..',
+}) {
+	const fs = require('fs')
+	const search = fs.readdirSync(parent)
+	return search.includes(fileOrDir)
+}
+
+function findSite(path = '..') {
+	if (findInParent({
+		fileOrDir: 'site',
+		parent: path,
+	})) {
+		return `${path}/site`
+	} else {
+		if (findInParent({
+			fileOrDir: 'etc',
+			parent: `../${path}`,
+		})) throw new Error('Reached root /')
+		return findSite(`../${path}`)
+	}
+}
